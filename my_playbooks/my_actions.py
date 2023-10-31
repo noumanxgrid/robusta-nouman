69d757174be2232f4b521be83c46aa7495160436
noumanxgrid <Nomi123nouman>
2023-10-31 12:56:42 -0400

    change

--- my_playbooks/my_actions.py
+++ my_playbooks/my_actions.py
@@ -4,7 +4,7 @@ from robusta.api import *
 def my_action(event: PodEvent):
     # we have full access to the pod on which the alert fired
     pod = event.get_pod()
-    pod_name = pod.metadata.name
+    pod_name = pod.metadata.namespace
     pod_logs = pod.get_logs()
     pod_processes = pod.exec("ps aux")
 
